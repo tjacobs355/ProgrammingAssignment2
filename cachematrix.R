6dc422b4efad2d5455acb9df29abcadf0d7bcf2f
tjacobs355 <tjacobs355@gmail.com>
2014-07-26 17:08:04 -0500

    completed prog assign2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions enable the caching of matrix inversion
+## result so that the inversion computation does not have
+## to be done if a particular matrix has already been
+## inverted.
 
-## Write a short comment describing this function
+## This function creates a special matrix object that caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        invM <- NULL
+        set <- function(y) {
+                x <<- y
+                invM <<- NULL
+                
+        }
+        get <- function() x
+        setInv <- function(solve) invM <<- solve
+        getInv <- function() invM
 
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special matrix created by
+##  the previous function. If the inverse already claculated then retrieves
+##  the cached inverse instead of re-calculating.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Check to see if inverse already calculated. If yes, then just return cached inverse
+        invM <- x$getInv()
+        if(!is.null(invM)) {
+                message("returning cached data")
+                return(invM)
+        }
+        
+        #If inverse was not already calculated then need to do the calc and return the inverse
+        data <- x$get()
+        invM <- solve(data, ...)
+        x$setInv(invM)
+        invM
+        
 }
+
+## Some other comments for my own understanding of these functions
+##   - when call makeCacheMatrix passing it a matrix M, it stores the matrix. It does not calculate
+##   -   the inverse, just a function to calculate the inverse. h <- makeCacheMatrix(M), then
+##   -   h$get() is the stored matrix.
+
